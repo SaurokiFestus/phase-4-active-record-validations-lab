48822fcf1f5199e1d08222e1c7cadc0ce1c26fc0
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-09-29 00:23:25 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :post_content, length: {minimum: 250}
+    validates :post_summary, length: {maximum: 250}
+
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
 end
